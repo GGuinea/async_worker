91fa184a118f4a7408d2749068bfebe7a417a18c
gguinea <kamil5176@gmail.com>
2023-12-21 09:49:42 +0100

    All together setup in main

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"async_worker/config"
+	"async_worker/internal"
+	"async_worker/internal/backgroundJobs/workers"
+	jobprocessor "async_worker/internal/pkg/jobProcessor"
+	"async_worker/internal/pkg/jobProcessor/migrations"
+	"context"
+)
+
+func main() {
+	ctx := context.Background()
+
+	cfg := config.NewConfig()
+	compositionRoot := internal.NewCompositionRoot(ctx, cfg)
+	backgroundJob, err := setupBackgroundJobProcessor(ctx, cfg, compositionRoot)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func setupBackgroundJobProcessor(ctx context.Context, cfg *config.Config, deps *internal.CompositionRoot) (*jobprocessor.JobProcessorClient, error) {
+	err := migrations.PerformStartupRiverMigration(ctx, deps.DbPool)
+	if err != nil {
+		panic(err)
+	}
+
+	backgroundWorkersMgmnt := workers.NewBackgroundJobWorkers(deps.BackgroundWorkers)
+	workers.AddDefaultWorker(backgroundWorkersMgmnt)
+
+	jobProcessorClient, err := jobprocessor.NewJobProcessorClient(ctx, deps, cfg.BackgroundProcessorConfig)
+	if err != nil {
+		return nil, err
+	}
+
+	return jobProcessorClient, nil
+
+}
